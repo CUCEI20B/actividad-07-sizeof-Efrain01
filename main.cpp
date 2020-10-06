358071cc85b22586a84e800e98db394d576ab1f3
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2020-10-06 11:28:35 -0500

    Modificacion de prueba_3

--- main.cpp
+++ main.cpp
@@ -8,13 +8,26 @@ int main() {
     if(caracter == "char"){
         cout<<sizeof(char)<<endl;
     }
-    getline(cin,entero);
+    /*getline(cin,entero);
     if(entero == "int"){
         cout<<sizeof(int)<<endl;
+    }
+    getline(cin,enterocorto);
+    if(enterocorto == "short"){
+        cout<<sizeof(short)<<endl;
+    }
+    getline(cin,flotante);
+    if(flotante == "float"){
+        cout<<sizeof(float)<<endl;
     }
     getline(cin,doble);
     if(doble == "double"){
         cout<<sizeof(double)<<endl;
     }
+    getline(cin,largo);
+    if(largo == "long"){
+        cout<<sizeof(long)<<endl;
+    }
+    */
     return 0;
 }
