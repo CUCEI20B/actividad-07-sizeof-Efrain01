99c0ef852b5f2ae754d51c683f56e325d6c54d25
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2020-10-06 08:47:42 -0500

    Prueba 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
-
+    string caracter, entero, enterocorto, flotante, doble, largo;
+    getline(cin,caracter);
+    if(caracter == "char"){
+        cout<<sizeof(char)<<endl;
+    }
+    getline(cin,entero);
+    if(entero == "int"){
+        cout<<sizeof(int)<<endl;
+    }
+    getline(cin,enterocorto);
+    if(enterocorto == "short"){
+        cout<<sizeof(short)<<endl;
+    }
+    getline(cin,flotante);
+    if(flotante == "float"){
+        cout<<sizeof(float)<<endl;
+    }
+    getline(cin,doble);
+    if(doble == "double"){
+        cout<<sizeof(double)<<endl;
+    }
+    getline(cin,largo);
+    if(largo == "long"){
+        cout<<sizeof(long)<<endl;
+    }
     return 0;
 }
